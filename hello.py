c348c694639f4b871edcfe9ae95f5db6c429eb6a
JorgePC <jorge.pecharroman@gmail.com>
2025-11-21 13:04:22 +0100

    otra prueba con comentarios de varias lineas con comilla simple

--- hello.py
+++ hello.py
@@ -3,7 +3,13 @@ print("¡Hola, mundo!")
 
 """Este es un comentario
 de varias líneas"""
+
 print("¡Bienvenido a Python!")
 
 # Otra línea de código
 print("¡Aprendiendo a programar!")
+
+''' Los comentarios de varias lineas
+también pueden hacerse con comillas simples '''
+print("¡Diviértete codificando!")
+
